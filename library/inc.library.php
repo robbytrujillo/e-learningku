aef52c41c1ffe6e8bc44504d86b8bb817aae27ea
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-30 06:43:18 +0700

    elearningku | fungsi library

---
+++ library/inc.library.php
@@ -0,0 +1,26 @@
+<?php
+date_default_timezone_set("Asia/Jakarta");
+
+function buatKode($tabel, $inisial)
+{
+    $struktur = mysqli_query("SELECT * FROM $tabel");
+    $field = mysqli_field_name($struktur, 0);
+    $panjang = mysqli_field_len($struktur, 0);
+    $qry = mysqli_query("SELECT MAX(" . $field . ") FROM " . $tabel);
+    $row = mysqli_fetch_array($qry);
+
+    if ($row[0] == "") {
+        $angka = 0;
+    } else {
+        $angka = substr($row[0], strlen($inisial));
+    }
+
+    $angka++;
+    $angka = strval($angka);
+    $tmp = "";
+    for ($i; $i <= ($panjang - strlen($inisial) - strlen($angka)); $i++) {
+        $tmp = $tmp . "0";
+    }
+    return $inisial . $tmp . $angka;
+}
+?>
