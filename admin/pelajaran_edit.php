e47fdec3fbad9774a99be096d368a7716add3c75
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-03 22:22:25 +0700

    elearningku | pelajaran  edit

--- admin/pelajaran_edit.php
+++ admin/pelajaran_edit.php
@@ -21,6 +21,61 @@ if (isset($_POST['btnSimpan'])) {
     if (count($pesanError) >= 1) {
         echo "<div class='mssgBox'>";
         echo "<img src='../images/attention.png'> <br><hr>";
+        $noPesan = 0;
+        foreach ($pesanError as $index => $pesan_tampil) {
+            $noPesan++;
+            echo "&nbsp; $noPesan. $pesan_tampil<br>";
+        }
+        echo "</div><br>";
+    } else {
+        //Skrip Simpan data ke Database
+        $txtKode = $_POST['txtKode'];
+        $mySql = "UPDATE pelajaran SET nm_pelajaran = '$txtNama' WHERE 
+            kd_pelajaran = $txtKode'";
+
+        $myQry = mysqli_query($mySql, $koneksidb) or die("Gagal query" . mysqli_error());
+        if ($myQry) {
+            echo "<meta http-equiv='refresh' content='0; url=?open=Pelajaran-Data'>";
+        }
+        exit;
     }
 }
+
+#Membaca Data Dari Database Untuk DiEdit
+$Kode = $_GET['Kode'];
+$mySql = "SELECT * FROM pelajaran WHERE kd_pelajaran='$Kode'";
+$myQry = mysqli_query($mySql, $koneksidb) or die("Query salah : " . mysqli_error());
+$myData = mysqli_fetch_array($myQry);
+
+//Membuat variabel isi ke form
+$dataKode = $myData['kd_pelajaran'];
+$dataNama = isset($_POST['txtNama']) ? $_POST['txtNama'] : $myData['nm_pelajaran'];
 ?>
+
+<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" name="form1" target="_self" id="form1">
+    <table class="table-list" width="650" border="0" cellspacing="1" cellpadding="3">
+        <tr>
+            <td bgcolor="#CCCCCC"><strong>UBAH PELAJARAN</strong></td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td width="183">Kode</td>
+            <td width="6">:</td>
+            <td width="439"><input name="textfield" type="text" value="<?php echo $dataKode; ?>" size="10"
+                    maxlength="4" />
+                <input name="txtKode" type="hidden" id="txtKode" value="<?php echo $dataKode; ?>" />
+            </td>
+        </tr>
+        <tr>
+            <td>Nama Pelajaran</td>
+            <td>:</td>
+            <td><input name="txtNama" type="text" id="txtNama" value="<?php echo $dataNama; ?>" size="70" maxlength="100" /></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td><input name="btnSimpan" type="submit" id="btnSimpan" value="Simpan" /></td>
+        </tr>
+    </table>
+</form>
