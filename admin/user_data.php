9a0ffddb05d9a90f4deb225a4d7b9d5092a3f57d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-09 06:49:32 +0700

    elearningku | manajemen user - user data

---
+++ admin/user_data.php
@@ -0,0 +1,10 @@
+<?php
+//validasi login user
+include_once "../library/inc.seslogin.php";
+//koneksi ke database Mysql
+include_once "../library/inc.connection.php"
+?>
+
+<table width="650" border="0" cellpadding="2" cellspacing="1" class="table-border">
+    
+</table>
