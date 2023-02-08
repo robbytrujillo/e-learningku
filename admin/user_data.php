365569c6b11a72da98b7591c69ab0fbddcd2554b
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-09 06:57:17 +0700

    elearningku | manajemen user - user data

--- admin/user_data.php
+++ admin/user_data.php
@@ -6,5 +6,44 @@ include_once "../library/inc.connection.php"
 ?>
 
 <table width="650" border="0" cellpadding="2" cellspacing="1" class="table-border">
-    
+<tr>
+    <td width="789" colspan="2"><h1><b>DATA USER  </b></h1></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="right"><a href="?open=User-Add" target="_self"><img src="../images/btn_add_data.png" height="30" border="0"  /></a></td>
+  </tr>
+  
+  <tr>
+    <td colspan="2">
+	<table class="table-list" width="100%" border="0" cellspacing="1" cellpadding="3">
+      <tr>
+        <td width="5%" bgcolor="#CCCCCC"><strong>No</strong></td>
+        <td width="9%" bgcolor="#CCCCCC"><strong>Kode</strong></td>
+        <td width="47%" bgcolor="#CCCCCC"><strong>Nama User </strong></td>
+        <td width="21%" bgcolor="#CCCCCC"><strong>Username</strong></td>
+        <td colspan="2" align="center" bgcolor="#CCCCCC"><strong>Tools</strong></td>
+        </tr>
+</table>
+<?php
+// Skrip menampilkan data User
+$mySql 	= "SELECT * FROM user ORDER BY kd_user ASC";
+$myQry 	= mysql_query($mySql, $koneksidb)  or die ("Query  salah: ".mysql_error());
+$nomor  = 0; 
+while ($myData = mysql_fetch_array($myQry)) {
+	$nomor++;
+	$Kode = $myData['kd_user'];
+?>
+
+  <tr>
+	<td> <?php echo $nomor; ?> </td>
+	<td> <?php echo $myData['kd_user']; ?> </td>
+	<td> <?php echo $myData['nm_user']; ?> </td>
+	<td> <?php echo $myData['username']; ?> </td>
+	<td width="9%" align="center"><a href="?open=User-Delete&Kode=<?php echo $Kode; ?>" target="_self" onclick="return confirm('YAKIN INGIN MENGHAPUS DATA USER INI ... ?')"> Delete</a></td>
+	<td width="9%" align="center"><a href="?open=User-Edit&Kode=<?php echo $Kode; ?>" target="_self">Edit</a></td>
+  </tr>
+	  
+<?php } ?>
+    </table></td>
+  </tr>
 </table>
