1524cb9c1f68ff054ea8c48804f5447c2fe2e26e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-11 11:55:10 +0700

    elearningku | manajemen guru - guru  add

--- admin/guru_edit.php
+++ admin/guru_edit.php
@@ -7,7 +7,7 @@ include_once "../library/inc.seslogin.php";
 include_once "../library/inc.library.php";
 
 # Skrip Tombol Simpan DiKlik
-if (isset($_POST['btnSimpan']) {
+if (isset($_POST['btnSimpan'])) {
     // Baca Data dari Form Input
     $txtNama = $_POST['txtNama'];
     $cmbKelamin = $_POST['cmbKelamin'];
@@ -18,22 +18,54 @@ if (isset($_POST['btnSimpan']) {
 
     // Validasi Form Inputs
     $pesanError = array();
-    if ($trim(txNama)==)"") {
+    if ($trim(txNama)=="") {
         $pesanError[] = "Data <b>Nama Kelas</b> tidak boleh kosong !";
     }
-    if ($trim(cmbKelamin)==)"") {
+    if ($trim(cmbKelamin)=="") {
         $pesanError[] = "Data <b>Kelaman</b> belum ada yang dipilih !";
     }
-    if ($trim(txAlamat)==)"") {
+    if ($trim(txAlamat)=="") {
         $pesanError[] = "Data <b>Alamat</b> tidak boleh kosong !";
     }
-    if ($trim(txNoTelepon)==)"") {
+    if ($trim(txNoTelepon)=="") {
         $pesanError[] = "Data <b>No. Telepon</b> tidak boleh kosong !";
     }
-    if ($trim(txUsername)==)"") {
+    if ($trim(txUsername)=="") {
         $pesanError[] = "Data <b>Username</b> tidak boleh kosong !";
     }
 
-    // 
-})
+    // Menampilkan Pesan Error dari Form
+    if (count($pesanError)>=1 ) {
+        echo "<div class='mssgBox'>";
+        echo "<img src='../images/attention.png'> <br><hr>";
+        $noPesan = 0;
+        foreach ($pesanError as $indeks=>$pesan_tampil) {
+        $noPesan++;
+            echo "&nbsp; $noPesan. $pesan_tampil<br>";
+        }
+        echo "</div> <br>";
+    }
+    else {
+        // Skrip simpan data ke Database
+        $kodeBaru = buatKode("guru", "G");
+        $mySql = "INSERT INTO guru (kd_guru, nm_guru, kelamin, alamat, no_telepon, username, password) 
+        VALUES ('$kodeBaru', '$txtNama', '$cmbKelamin', '$txtAlamat', '$txtNoTelepom',
+        '$txtUsername', MD5('$txtPassword') )";
+        $myQry = mysqli_query($mySql, $koneksidb) or die ("Gagal query".mysqli_error());
+        if($myQry) {
+            echo "<meta http-equiv='refresh' content='0;url=?open=Guru-Add'>";
+        }
+        exit;
+    }
+}
+
+// Membuat variabel isi ke form
+$dataKode = buatKode ("guru", "G");
+$dataNama = isset($_POST['txtNama']) ? $_POST['txtNama'] : '';
+$dataKelamin = isset($_POST['cmbKelamin']) ? $_POST['cmbKelamin'] : '';
+$dataKelamin = isset($_POST['txtAlamat']) ? $_POST['txtAlamat'] : '';
+$dataKelamin = isset($_POST['txtNoTelepon']) ? $_POST['txtNoTelepon'] : '';
+$dataKelamin = isset($_POST['txtUsername']) ? $_POST['txtUsername'] : '';
+$dataKelamin = isset($_POST['txtPassword']) ? $_POST['txtPassword'] : '';
 ?>
+
