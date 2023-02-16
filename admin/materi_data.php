f44c051c2e8a48a356f185980ebc129bfefb0fa6
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-17 06:50:50 +0700

    elearningku | manajemen materi belajar - materi data

--- admin/materi_data.php
+++ admin/materi_data.php
@@ -22,8 +22,51 @@ $mulai = $baris * ($hal-1);
     </tr>
     <tr>
         <td colspan="2" align="right">
-            <a href="?open=Materi-Add" target="_self"><img src="../images/btn_add_data.png" height="30" border="0" /></a>
+            <a href="?open=Materi-Add" target="_self"><img src="../images/btn_add_data.png" 
+            height="30" border="0" /></a>
         </td>
     </tr>
-    
+    <tr>
+        <td colspan="2">
+            <table class="table-list" width="100%" border="0" cellpadding="3" cellspacing="1">
+                <tr>
+                    <td width="4%" bgcolor="#CCCCCC"><strong>No</strong></td>
+                    <td width="8%" bgcolor="#CCCCCC"><strong>Kode</strong></td>
+                    <td width="44%" bgcolor="#CCCCCC"><strong>Nama Materi </strong></td>
+                    <td width="28%" bgcolor="#CCCCCC"><strong>Pelajaran </strong></td>
+                    <td colspan="2" align="center" bgcolor="#CCCCCC"><strong>Tools </strong></td>
+                </tr>
+            <?php
+            // Skirp menampilkan data materi
+            $mySql = "SELECT mb.*, pelajaran.nm_pelajaran FROM materi_belajar AS mb LEFT JOIN
+            pelajaran ON mb.kd_pelajaran = pelajaran.kd_pelajaran ORDER BY mb.kd_materi ASC LIMIT 
+            $mulai, $baris";
+            $myQry = mysqli_query($mySql, $koneksidb) or die ("Query salah : ".mysqli_error());
+            $nomor = $mulai;
+            while ($myData = mysqli_fetch_array($myQry)) {
+                $nomor++;
+                $Kode = $myData['kd_materi'];
+                ?>
+                <tr>
+                    <td> <?php echo $nomor; ?> </td>
+                    <td> <?php echo $myData['kd_materi']; ?> </td>
+                    <td> <?php echo $myData['nm_materi']; ?> </td>
+                    <td> <?php echo $myData['kd_pelajaran']; ?> </td>
+                    <td width="8%"><a href="?open=Materi-Delete&amp;Kode<?php echo $Kode; ?>" 
+                    target="_self">Delete</a></td>
+                    <td width="8%"><a href="?open=Materi-edit&amp;Kode<?php echo $Kode; ?>" 
+                    target="_self">Edit</a></td>
+                </tr>
+            <?php } ?>
+            </table>
+        </td>
+    </tr>
+    <td width="495"><strong>Jumlah Data :</strong> <?php echo $jumlah; ?></td>
+    <td width="194" align="right"><strong>Halaman Ke :</strong>
+	<?php
+	for ($h = 1; $h <= $maks; $h++) {
+		echo " <a href='?open=Materi-Data&hal=$h'>$h</a> ";
+	}
+	?> </td>
+  </tr>
 </table>
