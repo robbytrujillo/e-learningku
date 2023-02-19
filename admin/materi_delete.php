7da7b47c9cf468ac8b0c58fa7356c4a70c5d3bee
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-20 06:23:09 +0700

    elearningku | manajemen materi belajar - materi delete

--- admin/materi_delete.php
+++ admin/materi_delete.php
@@ -4,5 +4,31 @@ include_once "../library/inc.seslogin.php";
 // koneksi ke database MySQL
 include_once "../library/inc.connection.php";
 
+// Membaca variabel Kode pada URL (alamat browser)
+if (isset($_GET['Kode'])) {
+    $Kode = $_GET['Kode'];
 
+    // Membaca data file Materi
+    $mySql = "SELECT file_materi FROM materi_belajar WHERE kd_materi='$Kode'";
+    $myQry = mysqli_query($mySqli, $koneksidb) or die ("Query 1 salah : ".mysqli_error());
+    $myData = mysqli_fetch_array($myQry);
+
+    // jika file materi lama ada, dan akan dihapus
+    $fileMateri = $myData['file_materi'];
+    if(file_exists("../materi/".$fileMateri)) {
+        unlink("../materi/".$fileMateri);
+    }
+
+    // Hapus data sesuai Kode yang terbaca
+    $my2Sql = "DELETE FROM materi_belajar WHERE kd_materi='$Kode'";
+    $my2Qry = mysqli_query($my2Sql, $koneksidb) or die ("Error hapus data".mysqli_error());
+    if ($my2Qry) {
+        // Refresh halaman
+        echo "<meta http-equiv='refresh'content='0;url=?open=Materi-Data'>";
+    }
+}
+else {
+    // Jika tidak ada data Kode ditemukan di URL
+    echo "<b>Data yang dihapus tidak ada!</b>";
+}
 ?>
