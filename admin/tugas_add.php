3b949ed44c7b18bc8be6f503a39a6892e1fd726b
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-22 06:26:41 +0700

    elearningku | manajemen tugas belajar - tugas add

--- admin/tugas_add.php
+++ admin/tugas_add.php
@@ -49,7 +49,30 @@ if (isset($_POST['btnSimpan'])) {
         // Membuat kode Otomatis
         $kodeBaru = buatKode("tugas_belajar", "T");
 
+        # Skrip Upload file tugas
+        if (! empty($_FILES['txtNamaFile']['tmp_name'])) {
         
+        // Jika file tugas tidak kosong (ada tugas yang dipilih)
+        $nama_file = $_FILES['txtNamaFile']['name'];
+        $nama_file = stripcslashes($nama_file);
+        $nama_file = str_replace("'", "", $nama_file);
+
+        // Proses kopi tugas (menyimpan) ke folder
+        $nama_file = $kodeBaru.".".$nama_file;
+        copy($_FILES['txtNamaFile']['tmp_name'], "../tugas/ ".$nama_file);
+        } else {
+            // Jika file tugas tidak dipilih, maka simpan data kosong
+            $nama_file = "";
+        }
+
+        // Skrip simpan data ke database
+        $mySql = "INSERT INTO tugas_belajar (kd_tugas, nm_tugas, keterangan, 
+        file_tugas, kd_pelajaran, kd_kelas, kd_guru) VALUES ('$kodeBaru', 
+        '$txtNama', '$txtKeterangan', '$nama_file', '$cmbPelajaran', '$cmbKelas', 
+        '$cmbGuru')";
+
+        
+
     }
 }
 ?>
