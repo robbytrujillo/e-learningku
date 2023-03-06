f66345c71c44595f3ce1c10ef36d3f773ce053a2
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-06 14:04:17 +0700

    elearningku | laporan tugas

--- admin/laporan_tugas.php
+++ admin/laporan_tugas.php
@@ -19,5 +19,88 @@ if ($kodeKelas=="Semua") {
     $filterSQL = "WHERE tb.kd_kelas = '$KodeKelas'";
 }
 
-
+// Untuk pembagian halam data (Paging)
+$baris = 50;
+$hal = isset($_GET['hal']) ? $_GET['hal'] : 1;
+$infoSql = "SELECT * FROM tugas_belajar As tb $filterSQL";
+$infoQry = mysqli_query($infoSql, $koneksidb) or die ("error pagging: ".mysqli_error()); 
+$jumlah = mysqli_num_rows($infoQry);
+$maks = ceil($jumlah/$baris);
+$mulai = $baris * ($hal-1);
 ?>
+
+<h2> LAPORAN DATA TUGAS BELAJAR </h2>
+<form action="<?PHP $_SERVER['PHP_SELF']; ?>" method="post" name="form1">
+<table class="table-list" width="400" border="0" cellspacing="1" cellpadding="3">
+    <tr>
+        <td width="109" bgcolor="#CCCCCC"><strong>FILTER DATA</strong></td>
+        <td colspan="2">&nbsp;</td>
+    </tr>
+    <tr>
+        <td><strong>Kelas</strong></td>
+        <td width="7">:</td>
+        <td widtht="262"><select name="cmbKelas">
+            <option value="Semua">....</option>
+            <?php
+            // Skrip menampilkan data Kelas ke dalam List/Menu
+            $bacaSql = "SELECT * FROM kelas ORDER BY kd_kelas";
+            $bacaQry = mysqli_query($bacaSql, $koneksidb) or die ("Gagal Query".mysqli_error());
+            while ($bacaData = mysqli_fetch_array($bacaQry)) {
+                if ($bacaData['kd_kelas'] == $kodeKelas) {
+                    $cek = "selected";
+                } else {
+                    $cek = "";
+                }
+
+                echo "<option value='$bacaData[kd_kelas]' $cek $bacaData[nm_kelas] </option>";
+            }
+            ?>
+        </select>
+    <input name="btnTampil" type="submit" id="btnTampil" value="Tampil" />
+    </td>
+    </tr>
+</table>
+</form>
+
+<table class="table-list" width="700" border="0" cellspacing="1" cellpadding="3">
+    <tr>
+    <td width="4%" bgcolor="#CCCCCC"><strong>No</strong></td>
+    <td width="6%" bgcolor="#CCCCCC"><strong>Kode</strong></td>
+    <td width="34%" bgcolor="#CCCCCC"><strong>Nama Tugas </strong></td>
+    <td width="22%" bgcolor="#CCCCCC"><strong>Pelajaran</strong></td>
+    <td width="12%" bgcolor="#CCCCCC"><strong>Kelas</strong></td>
+    <td width="22%" bgcolor="#CCCCCC"><strong>Guru</strong></td>
+    </tr>
+    <?php
+// Skrip menampilkan data Tugas Belajar
+$mySql 	= "SELECT tb.*, pelajaran.nm_pelajaran, guru.nm_guru, kelas.nm_kelas 
+			FROM tugas_belajar AS tb
+			LEFT JOIN pelajaran ON tb.kd_pelajaran = pelajaran.kd_pelajaran 
+			LEFT JOIN guru ON tb.kd_guru = guru.kd_guru
+			LEFT JOIN kelas ON tb.kd_kelas = kelas.kd_kelas
+			$filterSQL ORDER BY tb.kd_tugas ASC LIMIT $mulai, $baris";
+$myQry 	= mysql_query($mySql, $koneksidb)  or die ("Query  salah : ".mysql_error());
+$nomor  = 0; 
+while ($myData = mysql_fetch_array($myQry)) {
+	$nomor++;
+?>
+
+  <tr>
+    <td><?php echo $nomor; ?> </td>
+    <td><?php echo $myData['kd_tugas']; ?> </td>
+    <td><?php echo $myData['nm_tugas']; ?> </td>
+    <td><?php echo $myData['nm_pelajaran']; ?> </td>
+    <td><?php echo $myData['nm_kelas']; ?></td>
+    <td><?php echo $myData['nm_guru']; ?></td>
+  </tr>
+<?php } ?>
+  <tr>
+    <td colspan="3"><strong>Jumlah Data :</strong> <?php echo $jumlah; ?></td>
+    <td colspan="3" align="right"><strong>Halaman Ke :</strong>
+      <?php
+	for ($h = 1; $h <= $maks; $h++) {
+		echo " <a href='?open=Laporan-Tugas&hal=$h&kodeKelas=$kodeKelas'>$h</a> ";
+	}
+	?></td>
+  </tr>
+</table>
