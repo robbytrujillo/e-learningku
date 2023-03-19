dc6b0fa8a84dc8842a2aa55ebab0cd614fb12564
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-19 07:35:26 +0700

    elearningku | guru - buka file - kontrol program login

--- guru/buka_file.php
+++ guru/buka_file.php
@@ -6,9 +6,27 @@ if (isset($_GET['open'])) {
         case '' :
             if(!file_exists ("info.php")) die ("File tidak ada !");
             include "info.php"; break;
+            
         case 'Halaman-Utama' :
             if(!file_exists ("info.php")) die ("File tidak ada !");
             include "info.php"; break;
+
+        # KONTROL PROGRAM LOGIN
+        case 'Login' :
+            if(!file_exists("info.php")) die ("File tidak ada !");
+            include "info.php"; break;
+
+        case 'Login-Validasi' :
+            if(!file_exists("login_validasi.php")) die ("File tidak ada !");
+            include "login_validasi.php"; break;
+
+        case 'Logout' :
+            if(!file_exists("logout.php")) die ("File tidak ada !");
+            include "logout.php"; break;
+
+        case 'Jadwal-Tampil' :
+            if(!file_exists("jadwal_tampil.php")) die ("File tidak ada !");
+            include "jadwal_tampil.php"; break;
     }
     
 }
